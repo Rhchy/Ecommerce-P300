1a6086f3dfa6fc58acb49eaddb793337e0c5fc8c
Rhchy <rhchyrony12@gmail.com>
2020-12-28 23:17:04 +0600

    First commit

---
+++ includes/feautre.php
@@ -0,0 +1,67 @@
+
+		<div class="home-welcome">
+			<div class="home-welcome-text" style="background-image: url(image/homebackgrndimg.png); height: 180px; ">
+				<h1 style="text-align: center; color:white"><b>Welcome To Chowdhury TECH</b></h1>
+				<h2 style="text-align: center;color:white">Largest Online Shopping In Bangladesh</h2>
+			</div>
+		</div>
+		<div class="home-prodlist">
+			<div>
+				<h3 style="text-align: center;color:white">Products Category</h3>
+			</div>
+			<div style="padding: 20px 30px; width: 85%; margin: 0 auto;" >
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/saree.php">
+                        <img src="./images/rony9.jpg" class="home-prodlist-imgi"height="240px" width="100%"></a>
+							</a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/perfume.php">
+                        <img src="./images/rony12.jpg" class="home-prodlist-imgi"height="240px" width="100%"></a>
+							</a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/hijab.php">
+                        <img src="./images/rony5.jpg" class="home-prodlist-imgi"height="240px" width="100%"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/toilatry.php">
+                        <img src="./images/rony6.jpg" class="home-prodlist-imgi"height="240px" width="100%"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/footwear.php">
+                        <img src="./images/rony10.jpg" class="home-prodlist-imgi"height="240px" width="100%"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left;">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/tshirt.php">
+                        <img src="./images/rony11.jpg" class="home-prodlist-imgi"height="240px" width="100%"></a>
+						</div>
+					</li>
+				</ul>
+				<ul style="float: left; ">
+					<li style="float: left; padding: 25px;">
+						<div class="home-prodlist-img"><a href="women/watch.php">
+                        <img src="./images/rony12.jpg" class="home-prodlist-imgi"height="240px" width="100%"></a>
+						</div>
+					</li>
+				</ul>
+				
+			</div>			
+        </div>
+    </div>        
